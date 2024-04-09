e97036470808a6e96e1d24dffd70f5aa52d14eb3
Harshaa-R-S <harshaars21it@psnacet.edu.in>
2024-04-09 10:16:14 +0530

    strings

---
+++ com/company/strings.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class strings {
+    
+    public static void Main (String[] args){
+        String s="Harshaa R S";
+        System.out.println(s);
+        System.out.println(s.charAt(7));
+        System.out.println(s.charAt(4));
+
+
+    }
+    
+}
